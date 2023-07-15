343331576607ad05b180356547318bf278aa4a3d
WallefJhone <139632433+WallefJhone@users.noreply.github.com>
2023-07-15 10:26:52 -0300

    Add files via upload

---
+++ Wallef.js
@@ -0,0 +1,5 @@
+let nota1 = Number(prompt("informe sua nota:"))
+let nota2 = Number(prompt("informe sua nota:"))
+let nota3 = Number(prompt("informe sua nota:"))
+let media = (nota1+nota2+nota3)/3
+console.log(media)
---
+++ Wallef1.js
@@ -0,0 +1,3 @@
+let temperatura = Number(prompt("informe a temperatura em fahrenheit:"))
+let C= ((temperatura - 32)*5)/9
+console.log(C)
