df5f60671ac0284cf6f35f2c77669c24c4a92fe3
WallefJhone <139632433+WallefJhone@users.noreply.github.com>
2023-07-15 11:24:28 -0300

    Add files via upload

---
+++ wallef5.js
@@ -0,0 +1,12 @@
+let nume = Number(prompt("informar o primeiro número inteiro:"))
+let nume1 = Number(prompt("informar o segundo número inteiro:"))
+let nume2 = Number(prompt("informar o primeiro número inteiro:"))
+let maior = 0
+if (nume > maior){
+    maior = nume
+} if (nume1 > maior){
+   maior = nume1
+} if (nume2 > maior){
+    maior = nume2
+}
+console.log(maior)
