a0215481d9c323e28315f981e0c3b1fd11406e90
WallefJhone <139632433+WallefJhone@users.noreply.github.com>
2023-07-28 16:37:21 -0300

    Add files via upload

---
+++ eu.q.js
@@ -0,0 +1,10 @@
+let eu = {
+    nome: "wallef",
+    idade: "22",
+    sexo: "masculino",
+    endereço: "travessa da verdade",
+}
+console.log(eu.nome)
+console.log(eu.idade)
+console.log(eu.sexo)
+console.log(eu.endereço)
